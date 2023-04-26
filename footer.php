fbd60856340b5aa7b7da07bde8442a85a5b123f6
Harshit  Satyanarayan Prasad <king16harshit@gmail.com>
2023-04-26 13:13:29 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,47 @@
+<footer class="text-center bg-light" data-aos="fade" id="footer" data-bg="white">
+            <div class="container py-5">
+                <div class="logo-footer">
+                    <!-- -small, -medium, -big -->
+                    <div class="">
+                        <img src="assets/images/logo.png" width="300" alt="">
+                    </div>
+                    <p class="lead">
+                    <div class="s-footer--additional col-12 contact p-0">
+                        <ul class="list-inline">
+                            
+                            <li class="d-sm-block d-lg-inline mb-sm-3 mb-lg-0 mr-sm-0 mr-lg-4"><i
+                                    class="mx-2 fas fa-map-pin"></i>Greater Jaipur Area, India</li> | 
+                            <li class="d-sm-block d-lg-inline"><i
+                                    class="mx-2 far fa-envelope-open"></i>contact@aerocraft.co.in</li>
+                        </ul>
+                    </div>
+                    </p>
+                </div>
+                <hr class="my-5">
+                <div class="row">
+                    <div class="col-12 down">
+                        <div class="wrapper justify-content-md-between align-items-md-center">
+                            <div class="link">
+                                <div class="list-element--text--inline">
+                                    <ul>
+                                        <li><a href="privacy.html">privacy policy</a></li>
+                                        <li><a href="privacy.html">cookie policy</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="text">
+                                <p>© 2022 Aerocraft Aviation</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!--  END Footer  -->
+    </div>
+    <script src="assets/js/master-dist.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, minimum-scale=1, initial-scale=1, shrink-to-fit=no" />
+
+    <title>
+        Aerocraft
+    </title>
+
+    <meta name="format-detection" content="telephone=no" />
+    <meta name="msapplication-tap-highlight" content="no" />
+    <meta name="theme-color" content="#000" />
+
+    <meta name="description" content="" />
+    <meta name="author" content="Patrick David" />
+
+    <!-- Favicons -->
+    <link rel="icon shortcut" href="assets/images/favicon-32.png" sizes="16x16" type="image/png" />
+    <link rel="icon" href="assets/images/favicon-32.png" sizes="32x32" type="image/png" />
+    <link rel="icon" href="assets/images/favicon-48.png" sizes="48x48" type="image/png" />
+    <link rel="icon" href="assets/images/favicon-62.png" sizes="62x62" type="image/png" />
+    <link rel="icon" href="assets/images/favicon-96.png" sizes="96x96" type="image/png" />
+
+    <!-- Canonical -->
+    <link rel="canonical" href="/" />
+
+    <!-- Open Graph -->
+    <meta property="og:image" content="" />
+    <meta property="og:title" content="" />
+    <meta property="og:description" content="" />
+    <meta property="og:url" content="" />
+
+    <!-- Twitter -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="" />
+    <meta name="twitter:creator" content="" />
+    <meta name="twitter:url" content="" />
+    <meta name="twitter:title" content="" />
+    <meta name="twitter:description" content="" />
+    <meta name="twitter:image" content="" />
+
+    <!-- Custom css -->
+    <link rel="stylesheet" href="assets/css/bootstrap.css" />
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="assets/css/all.min.css" />
+</head>
+
+<body>
+    <!--  Main Wrap  -->
+    <div id="main-wrap mb-5">
+        <!--  Header & Menu  -->
+        <!--  class: fixed, bg, white, transparent  -->
+        <header id="header" class="fixed--bg--white">
+            <!--  padding-sm, padding-md, padding-lg  -->
+            <div class="wrap-header--padding-sm">
+                <nav class="main-navigation">
+                    <!--  Header Logo  -->
+                    <!--  class: height-sm, height-md, height-lg  -->
+                    <div id="logo" class="height-lg">
+                        <a class="navbar-brand" href="index.php">
+
+                            <img src="assets/images/logo.png" alt="Logo">
+
+                        </a>
+                    </div>
+                    <!--  END Header Logo  -->
+                    <!--  Menu  -->
+                    <div id="main-menu" aria-expanded="false" role="navigation">
+                        <div class="menu-holder" data-hidden="lg">
+                            <ul>
+                                <!--  dropdown menu  -->
+                                <li>
+                                    <a href="index.php">Home</a>
+                                </li>
+                                <!-- <li class="dropdown-parent--shadow--round--icon--bgitems">
+                                    <a href="#">Tools</a>
+                                    <div class="dropdown-inner">
+                                        <ul>
+                                            <li><a href="tools.php#wait-time">Wait Time</a></li>
+                                            <li><a href="tools.php#search">Search Flight</a></li>
+                                            <li><a href="tools.php#notify">Notify Flight</a></li>
+                                        </ul>
+                                    </div>
+                                </li> -->
+                                <li>
+                                    <a href="products.php">Products</a>
+                                </li>
+                                <li>
+                                    <a href="about.php">About</a>
+                                </li>
+                                <li>
+                                    <a href="contact.php">Contact</a>
+                                </li>
+
+
+                            </ul>
+                        </div>
+                        <div class="cta">
+                            <a href="#" class="btn--medium--round--border">login</a>
+                        </div>
+                        <div class="menu-button" data-visible="lg">
+                            <div class="icons">
+                                <span class="bar bar-1"></span>
+                                <span class="bar bar-2"></span>
+                                <span class="bar bar-3"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="mobile-menu" data-visible="lg" aria-expanded="false" role="navigation">
+                        <!-- class: -bg -->
+                        <div class="mobile-wrap">
+                            <!-- class: -dotted, -border -->
+                            <div class="main-links--border col-12">
+                                <ul>
+                                    <li>
+                                        <a href="#">Home</a>
+                                    </li>
+                                    <li class="dropdown-parent--icon" aria-expanded="false">
+                                        <a href="javascript:void(0)">Products</a>
+                                        <div class="dropdown-inner">
+                                            <ul>
+                                                <li><a href="index.html">Home Agency</a></li>
+                                                <li><a href="index-saas.html">Home Saas</a></li>
+                                                <li><a href="index-corporate.html">Home Corporate</a></li>
+                                                <li><a href="index-minimal.html">Home Minimal</a></li>
+                                            </ul>
+                                        </div>
+                                    </li>
+
+                                    <li>
+                                        <a href="#">About</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Contact</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Pricing</a>
+                                    </li>
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <!--  END Menu  -->
+                </nav>
+            </div>
+        </header>
